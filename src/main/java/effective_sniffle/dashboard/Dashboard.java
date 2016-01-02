cecfb2a4cb8ff0c3d0e573390ca6f0c1efa33cb9
Ricardo Peres <ricardosilva.peres@hotmail.com>
2016-01-02 01:44:31 +0000

    Create spring-boot application file, it's container package and respective package-info.

---
+++ src/main/java/effective_sniffle/dashboard/Dashboard.java
@@ -0,0 +1,16 @@
+package effective_sniffle.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Ricardo Peres
+ * @since 2016-01-01
+ */
+@SpringBootApplication
+public class Dashboard {
+    public static void main(String[] args) {
+        SpringApplication.run(Dashboard.class, args);
+    }
+}
---
+++ src/main/java/effective_sniffle/dashboard/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 
+ * @author Ricardo Peres
+ * @since 2016-01-01
+ */
+package effective_sniffle.dashboard;
